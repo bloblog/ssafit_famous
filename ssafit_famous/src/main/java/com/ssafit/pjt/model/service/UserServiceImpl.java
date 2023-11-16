000c5e3570ae060a063cf33badd6cc5df445829e
YUHYESEUNG <h103906@naver.com>
2023-11-16 11:12:39 +0900

    [Feat] user dao 구현 및 mapper 세팅

--- ssafit_famous/src/main/java/com/ssafit/pjt/model/service/UserServiceImpl.java
+++ ssafit_famous/src/main/java/com/ssafit/pjt/model/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.ssafit.pjt.model.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ssafit.pjt.model.dao.ReviewDao;
+import com.ssafit.pjt.model.dao.UserDao;
 import com.ssafit.pjt.model.dto.User;
 
 @Service
