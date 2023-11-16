6133be9db0bdf460a288e28017bdf829fbccd5da
myeongsoyi <myeongsoyi@gmail.com>
2023-11-16 11:39:30 +0900

    [Feat] schedule, study, todo Service 구현

--- ssafit_famous/src/main/java/com/ssafit/pjt/model/service/StudyService.java
+++ ssafit_famous/src/main/java/com/ssafit/pjt/model/service/StudyService.java
@@ -1,5 +1,14 @@
 package com.ssafit.pjt.model.service;
 
-public interface StudyService {
+import com.ssafit.pjt.model.dto.Study;
 
+public interface StudyService {
+	// 일정 생성
+	int createStudy(Study study);
+	// 일정 수정
+	int modifyStudy(Study study, String loginUserId);
+	// 일정 삭제
+	int removeStudy(int studyKey, String loginUserId);
+	// 일정 상세
+	Study studyDetail(int studyKey);
 }
