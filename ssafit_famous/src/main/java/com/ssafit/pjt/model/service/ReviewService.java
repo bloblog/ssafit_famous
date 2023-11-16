615b7be7e2c05e47ad446e1e7196675082374505
YUHYESEUNG <h103906@naver.com>
2023-11-16 11:49:48 +0900

    [Feat] review service 및 serviceImpl 구현

--- ssafit_famous/src/main/java/com/ssafit/pjt/model/service/ReviewService.java
+++ ssafit_famous/src/main/java/com/ssafit/pjt/model/service/ReviewService.java
@@ -1,5 +1,21 @@
 package com.ssafit.pjt.model.service;
 
-public interface ReviewService {
+import java.util.List;
 
+import com.ssafit.pjt.model.dto.Review;
+import com.ssafit.pjt.model.dto.SearchCondition;
+
+public interface ReviewService {
+	
+	public int writeReview(Review review);
+	
+	public int modifyReview(Review review, String loginUserId);
+	
+	public int removeReview(int reviewKey, String loginUserId);
+	
+	public Review getReview(int reviewKey);
+	
+	public List<Review> searchReview(SearchCondition condition);
+	
+	
 }
