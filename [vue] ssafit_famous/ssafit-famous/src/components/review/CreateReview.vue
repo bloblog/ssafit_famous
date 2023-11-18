e29dcdeaac54280844ffda3d6f86f6556cc18976
myeongsoyi <myeongsoyi@gmail.com>
2023-11-18 19:39:30 +0900

    [Feat] CreateReview, ReviewDeatail 생성 및 구현

---
+++ [vue] ssafit_famous/ssafit-famous/src/components/review/CreateReview.vue
@@ -0,0 +1,36 @@
+<!-- 회고 작성하기 -->
+<template>
+    <div>
+        <h2>회고 작성하기</h2>
+        <form>
+            <div>
+                <!-- 아래 3개는 값 고정 -->
+                <label for="studyName">스터디명</label>
+                <input type="text" name="studyName"><br/>
+                <label for="studyCategory">카테고리</label>
+                <input type="text" name="studyCategory"><br/>
+                <label for="reviewDate">작성일</label>
+                <input type="text" name="reviewDate"><br/>
+                <label for="reviewImg">대표이미지</label>
+                <input type="file" name="reviewImg"/>
+                <!-- 파일 인풋 어떻게 하더라 -->
+            </div>
+            <div>
+                <label for="reviewTitle">제목</label>
+                <input type="text" name="reviewTitle"><br/>
+                <label for="reviewContent">내용</label>
+                <input type="textarea" name="reviewContent">
+            </div>
+            <input type="submit" value="작성완료">
+        </form>
+        <button>취소</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ [vue] ssafit_famous/ssafit-famous/src/components/review/ReviewDetail.vue
@@ -0,0 +1,20 @@
+<!-- 리뷰 상세 보기 -->
+<template>
+    <div>
+        <h2>리뷰 상세 보기</h2>
+        <div>
+            <h3>제목 : 최고의 스터디 짱짱</h3>
+            <p>작성자 : 최승준 작성일 : 2023.11.18 스터디명 : 최승준과 아이들 카테고리 : 운동</p>
+            <p>우리 스터디 짱이야 최고야 어쩌구 저쩌구</p>
+        </div>
+        <button>닫기</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
