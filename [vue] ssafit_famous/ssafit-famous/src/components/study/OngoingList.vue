89a8498deb32a2d4057a0b6f69f3c07bd0f6995b
myeongsoyi <myeongsoyi@gmail.com>
2023-11-18 17:36:33 +0900

    [Feat] MyInfoView 및 하위 view 구현

--- [vue] ssafit_famous/ssafit-famous/src/components/study/OngoingList.vue
+++ [vue] ssafit_famous/ssafit-famous/src/components/study/OngoingList.vue
@@ -1,1 +1,42 @@
 <!-- 현재 진행중인 스터디 리스트 -->
+<template>
+    <div>
+        <h3>현재 진행중인 스터디</h3>
+        <p>스터디명 클릭 시 상세페이지로 이동합니다.</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>스터디명</th>
+                    <th>카테고리</th>
+                    <th>시작일</th>
+                    <th>종료일</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td><a href="#">빛나는 코드 마법사들</a></td>
+                    <td>코딩</td>
+                    <td>2023.09.30</td>
+                    <td>2023.12.31</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td><a href="#">최승준과 아이들</a></td>
+                    <td>운동</td>
+                    <td>2023.09.01</td>
+                    <td>2023.11.30</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- [vue] ssafit_famous/ssafit-famous/src/router/index.js
+++ [vue] ssafit_famous/ssafit-famous/src/router/index.js
@@ -1,10 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MyInfo from '../components/user/MyInfo.vue'
+import OngoingList from '../components/study/OngoingList.vue'
+import CompleteList from '../components/study/CompleteList.vue'
+import MyReview from '../components/review/MyReview.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/info',
+      name: 'info',
+      component: MyInfo
+    },
+    {
+      path: '/ongoing',
+      name: 'ongoing',
+      component: OngoingList
+    },
+    {
+      path: '/done',
+      name: 'done',
+      component: CompleteList
+    },
+    {
+      path: '/review',
+      name: 'review',
+      component: MyReview
+    },
   ]
 })
 
