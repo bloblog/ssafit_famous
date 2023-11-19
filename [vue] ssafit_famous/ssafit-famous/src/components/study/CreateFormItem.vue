5a279c0ad73660bdaf6c8f21227570208019a90f
HYESEUNG <h103906@naver.com>
2023-11-19 15:35:43 +0900

    [Feat] 회고 게시판 및 datepicker 추가

--- [vue] ssafit_famous/ssafit-famous/src/components/study/CreateFormItem.vue
+++ [vue] ssafit_famous/ssafit-famous/src/components/study/CreateFormItem.vue
@@ -1,1 +1,43 @@
-<!-- 스터디 생성 폼 중 왼쪽 -->
+
+
+<template>
+    <div>
+        <div>
+            <label>스터디명 </label>
+            <input v-model="studyName" placeholder="스터디 이름을 입력하세요"><br/>
+            <label>카테고리</label>
+            <div class="category">
+                <input type="radio" v-model="category" id="coding" name="category" value="coding">코딩
+                <input type="radio" v-model="category" id="fitness" name="category" value="fitness">운동
+                <input type="radio" v-model="category" id="hobby" name="category" value="hobby">취미
+                <input type="radio" v-model="category" id="reading" name="category" value="reading">독서
+            </div>
+            <label>시작일 </label>
+            <VueDatePicker v-model="startDate"></VueDatePicker><br/>
+            <label>종료일 </label>
+            <VueDatePicker v-model="endDate"></VueDatePicker><br/>
+            <label>알림주기 </label>
+            <input v-model="alarm" type="number" placeholder="몇 일 전에 알림을 드릴까요?"><br/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+import {ref} from 'vue'
+
+const studyName = ref(null);
+const category = ref(null);
+const startDate = ref(new Date());
+const endDate = ref(new Date());
+const alarm = ref(0);
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- [vue] ssafit_famous/ssafit-famous/src/views/CreateStudyView.vue
+++ [vue] ssafit_famous/ssafit-famous/src/views/CreateStudyView.vue
@@ -2,24 +2,7 @@
     <div>
         <h2>스터디 생성 폼</h2>
         <div class="create-form">
-            <div>
-                <label>스터디명 </label>
-                <input v-model="studyName" placeholder="스터디 이름을 입력하세요"><br/>
-                <label>카테고리</label>
-                <div class="category">
-                    <input type="radio" v-model="category" id="coding" name="category" value="coding">코딩
-                    <input type="radio" v-model="category" id="fitness" name="category" value="fitness">운동
-                    <input type="radio" v-model="category" id="hobby" name="category" value="hobby">취미
-                    <input type="radio" v-model="category" id="reading" name="category" value="reading">독서
-                </div>
-                <label>시작일 </label>
-                <input v-model="startDate" placeholder="캘린더 띄우기"><br/>
-                <label>종료일 </label>
-                <input v-model="endDate" placeholder="캘린더 띄우기"><br/>
-                <label>알림주기 </label>
-                <input v-model="alarm" type="number" placeholder="몇 일 전에 알림을 드릴까요?"><br/>
-            
-            </div>
+            <CreateFormItem/>
             <div class="v-line"></div>
             <div class="form-member">
                 <label>팀원 목록</label>
@@ -33,13 +16,7 @@
 
 <script setup>
 import SearchMemberItem from '@/components/user/SearchMemberItem.vue'
-import {ref} from 'vue'
-
-const studyName = ref(null);
-const category = ref(null);
-const startDate = ref(null);
-const endDate = ref(null);
-const alarm = ref(0);
+import CreateFormItem from '../components/study/CreateFormItem.vue';
 
 
 </script>
