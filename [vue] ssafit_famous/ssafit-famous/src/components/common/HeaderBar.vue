1d2ac1b3e4f120f00944e79f587c77afdb8e3f45
HYESEUNG <h103906@naver.com>
2023-11-19 13:31:13 +0900

    [Feat] 헤더 변경사항

--- [vue] ssafit_famous/ssafit-famous/src/components/common/HeaderBar.vue
+++ [vue] ssafit_famous/ssafit-famous/src/components/common/HeaderBar.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <header>
-            <router-link to="createStudy">스터디 만들기</router-link> <br/>
+            <router-link to="/createStudy">스터디 만들기</router-link> <br/>
             <a href="/nested/index.html">홈로고</a> <br/>
             <span class="MyPageDropdown">마이페이지 </span>
-            <router-link to="dashboard">대시보드 </router-link>
-            <router-link to="infoView">정보수정</router-link> <br/>
+            <router-link to="/dashboard">대시보드 </router-link>
+            <router-link to="/infoView">정보수정</router-link> <br/>
             <span>로그아웃 (alert)</span>
             <span>로그인 (modal)</span>
             <span>회원가입 (modal) </span>
