8ef1fde28ff9f078ea80d5f1027c34d2e1daf2ee
YUHYESEUNG <h103906@naver.com>
2023-11-22 08:55:58 +0900

    [Feat] mapper 수정

--- ssafit_famous/src/main/java/com/ssafit/pjt/config/WebConfig.java
+++ ssafit_famous/src/main/java/com/ssafit/pjt/config/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer{
 				.addResourceLocations("classpath:/META_INF/resources/webjars/springfox-swagger-ui/");
 	}
 	
+	
 //	@Override
 //	public void addInterceptors(InterceptorRegistry registry) {
 //		registry.addInterceptor(new LoginCheckInterceptor()).addPathPatterns("/api/review/**");
@@ -35,7 +36,6 @@ public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE", "PUT"); // 이거 추가함
+		registry.addMapping("/**").allowedOrigins("*");
 	}
-	
 }
