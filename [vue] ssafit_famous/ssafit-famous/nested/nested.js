ff0da2f251355b3a1d87f2eefbb5796ad4500059
myeongsoyi <myeongsoyi@gmail.com>
2023-11-21 11:11:49 +0900

    [Feat] login 관련 state와 getter store로 이동 + header 기본 구현

--- [vue] ssafit_famous/ssafit-famous/nested/nested.js
+++ [vue] ssafit_famous/ssafit-famous/nested/nested.js
@@ -4,9 +4,17 @@ import { createPinia } from 'pinia'
 import App from './nestedApp.vue'
 import router from '../src/router'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia);
 app.use(router)
 
 app.mount('#app')
--- [vue] ssafit_famous/ssafit-famous/src/views/WelcomeView.vue
+++ [vue] ssafit_famous/ssafit-famous/src/views/WelcomeView.vue
@@ -14,7 +14,7 @@
             </div>
             <div id = "logIn">
                 <div class="c" v-if="store.status">
-                    <button type="button" class="btn" @click="signout">로그아웃할래</button>
+                    <button type="button" class="btn" @click="store.signout">로그아웃할래</button>
                     <a href="/nested/index.html">그냥 둘러볼래~</a>
                 </div>
                 <div class="c" v-else>
@@ -40,17 +40,17 @@
                         <form>
                             <div class="mb-3">
                                 <label for="userid" class="form-label">아이디</label>
-                                <input type="text" class="form-control" id="userid" v-model.lazy="id">
+                                <input type="text" class="form-control" id="userid" v-model.lazy="store.id">
                             </div>
                             <div class="mb-3">
                                 <label for="userpw" class="form-label">비밀번호</label>
-                                <input type="password" class="form-control" id="userpw" v-model.lazy="pw">
+                                <input type="password" class="form-control" id="userpw" v-model.lazy="store.pw">
                             </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="signout">취소</button>
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="signin">로그인</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="store.signout">취소</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="store.signin">로그인</button>
                     </div>
                     </div>
                 </div>
@@ -62,38 +62,36 @@
                     <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="signupModalLabel">회원가입</h1>
-                        
-                        
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <form>
                             <div class="mb-3">
                                 <label for="userid" class="form-label">아이디</label>
-                                <input type="text" class="form-control" id="userid" v-model.lazy="id">
-                                <button type="button" @click="idCheck">중복확인</button>
-                                <div v-if="validIdCheck">
-                                    <p v-if="!validId">이미 존재하는 아이디입니다.</p>
+                                <input type="text" class="form-control" id="userid" v-model.lazy="store.id">
+                                <button type="button" @click="store.idCheck">중복확인</button>
+                                <div v-if="store.validIdCheck">
+                                    <p v-if="!store.validId">이미 존재하는 아이디입니다.</p>
                                     <p v-else>사용할 수 있는 아이디입니다.</p>
                                 </div>
                             </div>
                             <div class="mb-3">
                                 <label for="userpw" class="form-label">비밀번호</label>
-                                <input type="password" class="form-control" id="userpw" v-model="pw" v-bind:disabled="!validId">
+                                <input type="password" class="form-control" id="userpw" v-model="store.pw" v-bind:disabled="!store.validId">
                             </div>
                             <div class="mb-3">
                                 <label for="userpwcheck" class="form-label">비밀번호 확인</label>
-                                <input type="password" class="form-control" id="userpwcheck" v-model="pwcheck" v-bind:disabled="!validId">
+                                <input type="password" class="form-control" id="userpwcheck" v-model="store.pwcheck" v-bind:disabled="!store.validId">
                             </div>
-                            <div v-if="pwcheck!=null">
-                                <p v-if="signinConfirm">비밀번호가 일치합니다.</p>
+                            <div v-if="store.pwcheck!=null">
+                                <p v-if="store.signinConfirm">비밀번호가 일치합니다.</p>
                                 <p v-else>비밀번호가 일치하지 않습니다.</p>
                             </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="signout">취소</button>
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="signup" v-bind:disabled="!validId || !signinConfirm">회원가입</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="store.signout">취소</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="store.signup" v-bind:disabled="!store.validId || !store.signinConfirm">회원가입</button>
                     </div>
                     </div>
                 </div>
@@ -103,101 +101,8 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
-import axios from "axios";
 import { useLoginUserStore } from '../stores/loginUser';
-import { storeToRefs } from 'pinia';
-
-const id = ref();
-const pw = ref();
-const pwcheck = ref();
-const login = ref(false);
-const validId = ref(false);
-const validIdCheck = ref(false);
-const signinConfirm = ref(false);
 const store = useLoginUserStore();
-
-watch(pwcheck, ()=>{
-    if(pw.value === pwcheck.value){
-        signinConfirm.value = true;
-    }else{
-        signinConfirm.value = false;
-    }
-});
-
-const signout = function(){
-    id.value = null;
-    pw.value = null;
-    store.userId = undefined;
-    store.userKey = undefined;
-    store.status = false;
-}
-
-const signin = function(){
-    axios
-        .post('http://localhost:8080/api/login', {
-            "id": id.value,
-            "password": pw.value,
-        })
-        .then(function(response) {
-            console.log(response);
-            // 204 : id 혹은 pw가 틀렸습니다.
-            if(response.status === 204){
-                alert("아이디 혹은 비밀번호가 틀렸습니다.");
-                id.value = null;
-                pw.value = null;
-            }
-            // 200 : 로그인!
-            if(response.status === 200){
-                store.userId = id.value;
-                store.userKey = response.data;
-                store.status = true;
-                alert("로그인이 성공했습니다.");
-            }
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-};
-
-const signup = ref(function(){
-    axios
-        .post('http://localhost:8080/api/signup', {
-            "id": id.value,
-            "password": pw.value,
-        })
-        .then(function(response) {
-            console.log(response.status);
-            // 200 : 회원가입 성공!
-            alert("회원가입에 성공했습니다.");
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-    id.value = null;
-    pw.value = null;
-    pwcheck.value = null;
-});
-
-const idCheck = ref(function(){
-    axios
-        .get('http://localhost:8080/api/search/'+id.value)
-        .then(function(response) {
-            validIdCheck.value = true;
-            console.log(response.status);
-            // 200 : 이미 존재하는 아이디 입니다.
-            if(response.status === 200){
-                validId.value = false;
-            }
-            // 204 : 사용할 수 있는 아이디 입니다.
-            if(response.status === 204){
-                validId.value = true;
-            }
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-})
 
 </script>
 
